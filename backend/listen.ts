8acc24ef5ea1f7c259b3b874eb1a69d2dc298839
SaraGarciaM <saragarciamatos@gmail.com>
2024-07-26 10:26:27 +0100

    Removes unneded log

--- backend/listen.ts
+++ backend/listen.ts
@@ -4,12 +4,10 @@ import dotenv from "@dotenvx/dotenvx";
 dotenv.config();
 
 const PORT = process.env.PORT;
-const db = process.env.PGDATABASE;
 
 app
   .listen(PORT, (): void => {
     console.log(`Server running at port ${PORT}`);
-    console.log(db);
   })
   .on("error", (error) => {
     console.log(error.message);
