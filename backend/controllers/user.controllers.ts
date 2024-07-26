b2e6c3f765080427e99e161da28b1b263ceae9b4
SaraGarciaM <saragarciamatos@gmail.com>
2024-07-26 18:28:04 +0100

    Adds signIn controller

--- backend/controllers/user.controllers.ts
+++ backend/controllers/user.controllers.ts
@@ -1,10 +1,42 @@
 import { Response, Request, NextFunction } from "express";
-import { createUser } from "../models/user.models";
+import { createUser, fetchUser, logInUser } from "../models/user.models";
 
-export const postUser = (req: Request, res: Response, next: NextFunction) => {
-  const { username, email, password } = req.body;
+export const registerUser = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const {
+    username,
+    email,
+    password,
+  }: { username: string; email: string; password: string } = req.body;
 
   createUser(username, email, password)
+    .then((user) => {
+      res.status(200).send({ user });
+    })
+    .catch((error) => {
+      next(error);
+    });
+};
+
+export const signInUser = (req: Request, res: Response, next: NextFunction) => {
+  const { email, password }: { email: string; password: string } = req.body;
+
+  logInUser(email, password)
+    .then((user) => {
+      res.status(200).send({ user });
+    })
+    .catch((error) => {
+      next(error);
+    });
+};
+
+export const getUser = (req: Request, res: Response, next: NextFunction) => {
+  const { user_id } = req.params;
+
+  fetchUser(user_id)
     .then((user) => {
       res.status(200).send({ user });
     })
