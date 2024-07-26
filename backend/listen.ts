b8dc4cedabc6f8683b2ef3c46fe73e557fc2b915
SaraGarciaM <saragarciamatos@gmail.com>
2024-07-26 10:23:18 +0100

    Removes unnecessary code

--- backend/listen.ts
+++ backend/listen.ts
@@ -4,10 +4,12 @@ import dotenv from "@dotenvx/dotenvx";
 dotenv.config();
 
 const PORT = process.env.PORT;
+const db = process.env.PGDATABASE;
 
 app
   .listen(PORT, (): void => {
     console.log(`Server running at port ${PORT}`);
+    console.log(db);
   })
   .on("error", (error) => {
     console.log(error.message);
