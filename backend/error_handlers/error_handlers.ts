9abdac780dcea78f155584fb1cce4e28ee08f233
SaraGarciaM <saragarciamatos@gmail.com>
2024-07-26 18:28:41 +0100

    Fixes error code and ads other err cases

--- backend/error_handlers/error_handlers.ts
+++ backend/error_handlers/error_handlers.ts
@@ -9,7 +9,11 @@ export const sendCustomError: ErrorRequestHandler = (err, req, res, next) => {
 
 export const sendPsqlError: ErrorRequestHandler = (err, req, res, next) => {
   if (err.code === "23505") {
-    res.status(403).send({ message: "Already exists." });
+    res.status(409).send({ message: "Already exists." });
+  }
+
+  if (err.code === "22P02" || err.code === "23502") {
+    res.status(400).send({ message: "Bad request." });
   }
   next(err);
 };
