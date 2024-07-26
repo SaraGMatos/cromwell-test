beb9a824348bcf858be65992630a658795747dee
SaraGarciaM <saragarciamatos@gmail.com>
2024-07-26 13:34:18 +0100

    Creates mock data and updates seed funct

---
+++ backend/db/data.ts
@@ -0,0 +1,27 @@
+export const userData = [
+  {
+    username: "John23",
+    email: "john23@test.com",
+    password: "test",
+  },
+  {
+    username: "Lina",
+    email: "lina@test.com",
+    password: "test",
+  },
+  {
+    username: "Mike",
+    email: "mike@test.com",
+    password: "test",
+  },
+  {
+    username: "Diana",
+    email: "diana@test.com",
+    password: "test",
+  },
+  {
+    username: "Carmen",
+    email: "carmen@test.com",
+    password: "test",
+  },
+];
--- backend/db/seed.ts
+++ backend/db/seed.ts
@@ -1,14 +1,30 @@
 import { db } from "./connection";
+import format from "pg-format";
+import { userData } from "../db/data";
 
 export const seed = () => {
-  return db.query(`DROP TABLE IF EXISTS users;`).then(() => {
-    return db.query(
-      `CREATE TABLE users (
+  return db
+    .query(`DROP TABLE IF EXISTS users;`)
+    .then(() => {
+      return db.query(
+        `CREATE TABLE users (
       user_id SERIAL PRIMARY KEY, 
       username VARCHAR NOT NULL, 
       email VARCHAR NOT NULL, 
       password VARCHAR NOT NULL,
       UNIQUE (username, email));`
-    );
-  });
+      );
+    })
+    .then(() => {
+      const insertUsersQueryStr = format(
+        "INSERT INTO users (username, email, password) VALUES %L;",
+        userData.map(({ username, email, password }) => [
+          username,
+          email,
+          password,
+        ])
+      );
+
+      return db.query(insertUsersQueryStr);
+    });
 };
