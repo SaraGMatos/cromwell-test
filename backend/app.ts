5e6aa3f2fbb8eaf5c0097c5bdaf542001bdc55df
SaraGarciaM <saragarciamatos@gmail.com>
2024-07-26 12:43:58 +0100

    Adds some error handling middleware

--- backend/app.ts
+++ backend/app.ts
@@ -1,8 +1,16 @@
 import express, { Application } from "express";
 import { postUser } from "./controllers/user.controllers";
+import {
+  sendCustomError,
+  sendPsqlError,
+} from "./error_handlers/error_handlers";
 
 export const app: Application = express();
 
 app.use(express.json());
 
 app.post("/user/register", postUser);
+
+app.use(sendCustomError);
+
+app.use(sendPsqlError);
