6c962391d38bd6fab05113d60a7005563f6d3d15
SaraGarciaM <saragarciamatos@gmail.com>
2024-07-26 23:16:26 +0100

    Removes psql handler

--- backend/error_handlers/error_handlers.ts
+++ backend/error_handlers/error_handlers.ts
@@ -4,16 +4,12 @@ export const sendCustomError: ErrorRequestHandler = (err, req, res, next) => {
   if (err.status && err.message) {
     res.status(err.status).send({ message: err.message });
   }
-  next(err);
+
+  next(JSON.stringify(err));
 };
 
-export const sendPsqlError: ErrorRequestHandler = (err, req, res, next) => {
-  if (err.code === "23505") {
-    res.status(409).send({ message: "Already exists." });
-  }
+export const sendServerError: ErrorRequestHandler = (err, req, res, next) => {
+  console.error(err);
 
-  if (err.code === "22P02" || err.code === "23502") {
-    res.status(400).send({ message: "Bad request." });
-  }
-  next(err);
+  res.status(500).send({ message: "Internal Server Error" });
 };
