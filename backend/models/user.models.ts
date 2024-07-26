470333b17a803ca54c1500787068536f3ab06410
SaraGarciaM <saragarciamatos@gmail.com>
2024-07-26 10:56:08 +0100

    First real db query from model

--- backend/models/user.models.ts
+++ backend/models/user.models.ts
@@ -1,11 +1,19 @@
+import { db } from "../db/connection";
+
 export const createUser = (
   username: string,
   email: string,
   password: string
 ) => {
-  return {
-    username: username,
-    email: email,
-    password: password,
-  };
+  const formattedUser = [username, email, password];
+
+  return db
+    .query(
+      `INSERT INTO users (username, email, password)
+    VALUES ($1, $2, $3) RETURNING *`,
+      formattedUser
+    )
+    .then(({ rows }) => {
+      return rows[0];
+    });
 };
