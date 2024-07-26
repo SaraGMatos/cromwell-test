7fb3b1959455980196d1f8c59d1f72c6c65eb8c3
SaraGarciaM <saragarciamatos@gmail.com>
2024-07-26 23:15:46 +0100

    Adds logs and refactors some responses

--- backend/controllers/user.controllers.ts
+++ backend/controllers/user.controllers.ts
@@ -1,5 +1,5 @@
 import { Response, Request, NextFunction } from "express";
-import { createUser, fetchUser, logInUser } from "../models/user.models";
+import { createUser, fetchUserById, logInUser } from "../models";
 
 export const registerUser = (
   req: Request,
@@ -12,11 +12,20 @@ export const registerUser = (
     password,
   }: { username: string; email: string; password: string } = req.body;
 
+  console.info(
+    `Handling post method for registering user with email '${email}'`
+  );
+
   createUser(username, email, password)
-    .then((user) => {
-      res.status(200).send({ user });
+    .then((user_id) => {
+      // Resource creation is typically 201 in REST
+      console.info(`User with id '${user_id}' registered successfully`);
+
+      res.status(200).location(`/user/${user_id}`).send({ user: { user_id } });
     })
     .catch((error) => {
+      console.error(`Error occurred registering user with email '${email}'`);
+
       next(error);
     });
 };
@@ -24,23 +33,45 @@ export const registerUser = (
 export const signInUser = (req: Request, res: Response, next: NextFunction) => {
   const { email, password }: { email: string; password: string } = req.body;
 
+  console.info(`Handling post method for login user with email '${email}'`);
+
   logInUser(email, password)
-    .then((user) => {
-      res.status(200).send({ user });
+    .then((user_id) => {
+      console.info(`User with email '${email}' logged in successfully`);
+
+      res.status(200).send({ user: { user_id } });
     })
     .catch((error) => {
+      console.error(`Error occurred logging user with email '${email}' in`);
+
       next(error);
     });
 };
 
-export const getUser = (req: Request, res: Response, next: NextFunction) => {
+export const getUserById = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const { user_id } = req.params;
 
-  fetchUser(user_id)
+  console.info(`Handling get request for user with id '${user_id}'`);
+
+  const formattedId = Number(user_id);
+
+  if (!formattedId) {
+    return next({ status: 400, message: "Bad request." });
+  }
+
+  fetchUserById(formattedId)
     .then((user) => {
+      console.info(`User with id '${user_id}' fetched successfully`);
+
       res.status(200).send({ user });
     })
     .catch((error) => {
+      console.error(`Error occurred fetching user with id '${user_id}'`);
+
       next(error);
     });
 };
