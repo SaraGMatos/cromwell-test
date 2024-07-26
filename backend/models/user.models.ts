2eb0d8ebb171b605e253a3a41ae444fd0e359776
SaraGarciaM <saragarciamatos@gmail.com>
2024-07-26 18:29:30 +0100

    Adds fetch model

--- backend/models/user.models.ts
+++ backend/models/user.models.ts
@@ -7,6 +7,7 @@ export const createUser = (
 ) => {
   const formattedUser = [username, email, password];
 
+  //! Below is redundant? The following check should fire the error when checking the type of a missing field
   if (!username || !email || !password) {
     return Promise.reject({ status: 400, message: "Bad request." });
   }
@@ -22,10 +23,49 @@ export const createUser = (
   return db
     .query(
       `INSERT INTO users (username, email, password)
-    VALUES ($1, $2, $3) RETURNING *`,
+    VALUES ($1, $2, $3) RETURNING user_id`,
       formattedUser
     )
+    .then(({ rows: userId }) => {
+      return userId[0];
+    });
+};
+
+export const logInUser = (email: string, password: string) => {
+  //! Below is redundant? The following check should fire the error when checking the type of a missing field
+  if (!email || !password) {
+    return Promise.reject({ status: 400, message: "Bad request." });
+  }
+
+  if (typeof email !== "string" || typeof password !== "string") {
+    return Promise.reject({ status: 400, message: "Bad request." });
+  }
+
+  return db
+    .query(`SELECT user_id FROM users WHERE email = $1`, [email])
+    .then(({ rows: userId }) => {
+      if (userId.length === 0) {
+        return Promise.reject({ status: 404, message: "Not found." });
+      }
+
+      return db
+        .query(`SELECT password FROM users WHERE email = $1`, [email])
+        .then(({ rows: passwordObject }) => {
+          const givenPassword: string = passwordObject[0].password;
+          return givenPassword === password
+            ? userId[0]
+            : Promise.reject({ status: 400, message: "Bad request." });
+        });
+    });
+};
+
+export const fetchUser = (user_id: string) => {
+  return db
+    .query(`SELECT username, email FROM users WHERE user_id = $1`, [user_id])
     .then(({ rows }) => {
+      if (rows.length === 0) {
+        return Promise.reject({ status: 404, message: "Not found." });
+      }
       return rows[0];
     });
 };
