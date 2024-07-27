e114466035d8541fe228b170e4504b03bbcd6c5c
SaraGarciaM <saragarciamatos@gmail.com>
2024-07-27 13:28:13 +0100

    Expands component and adds some basic input validation

--- frontend/src/components/RegisterPage.tsx
+++ frontend/src/components/RegisterPage.tsx
@@ -1,4 +1,58 @@
+import {
+  ChangeEvent,
+  FormEvent,
+  SyntheticEvent,
+  useEffect,
+  useState,
+} from "react";
+import { createUser } from "../api";
+
 export function RegisterPage() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [usernameInputError, setUsernameInputError] = useState("");
+  const [emailInputError, setEmailInputError] = useState("");
+  const [passwordInputError, setPasswordInputError] = useState("");
+
+  function handleSubmit(event: FormEvent) {
+    event?.preventDefault();
+    createUser(username, email, password);
+  }
+
+  function handleUsernameBlur() {
+    if (username.length < 7) {
+      setUsernameInputError(
+        "Please make sure your username matches the requirements."
+      );
+    } else {
+      setUsernameInputError("");
+    }
+  }
+
+  function handleEmailBlur() {
+    const emailRegexPattern = /^[\w-\.]+@([\w-]+\.)+[\w]{2,4}$/g;
+    if (!emailRegexPattern.test(email)) {
+      setEmailInputError("Please make sure you are entering a valid email.");
+    } else {
+      setEmailInputError("");
+    }
+  }
+
+  function handlePasswordBlur() {
+    const passwordRegexPattern =
+      /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{10,}$/g;
+
+    if (!passwordRegexPattern.test(password)) {
+      setPasswordInputError(
+        "Please make sure your password matches the requirements."
+      );
+    } else {
+      setPasswordInputError("");
+    }
+  }
+
   return (
     <section className="h-full bg-white">
       <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
@@ -14,10 +68,10 @@ export function RegisterPage() {
         </div>
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form className="space-y-6" action="#" method="POST">
+          <form className="space-y-6" onSubmit={handleSubmit}>
             <div>
               <label
-                htmlFor="name"
+                htmlFor="username"
                 className="block text-sm font-medium leading-6 text-gray-900"
               >
                 Username
@@ -27,9 +81,25 @@ export function RegisterPage() {
                   id="username"
                   name="username"
                   type="text"
+                  value={username}
+                  onChange={(event: ChangeEvent<HTMLInputElement>) => {
+                    setUsername(event.target.value);
+                  }}
+                  onBlur={handleUsernameBlur}
                   required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  className="block w-full rounded-md border-0 py-1.5 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
+                {usernameInputError && (
+                  <p className="text-[13px] text-red-500">
+                    {usernameInputError}
+                  </p>
+                )}
+                <label
+                  htmlFor="username"
+                  className="block text-[12px] font-medium leading-6 text-gray-600"
+                >
+                  Please ensure your username is at least 7 letter long
+                </label>
               </div>
             </div>
 
@@ -45,10 +115,17 @@ export function RegisterPage() {
                   id="email"
                   name="email"
                   type="email"
-                  autoComplete="email"
+                  value={email}
+                  onChange={(event) => {
+                    setEmail(event.target.value);
+                  }}
+                  onBlur={handleEmailBlur}
                   required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  className="block w-full rounded-md border-0 py-1.5 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
+                {emailInputError && (
+                  <p className="text-[13px] text-red-500">{emailInputError}</p>
+                )}
               </div>
             </div>
 
@@ -66,10 +143,26 @@ export function RegisterPage() {
                   id="password"
                   name="password"
                   type="password"
-                  autoComplete="current-password"
+                  value={password}
+                  onChange={(event) => {
+                    setPassword(event.target.value);
+                  }}
+                  onBlur={handlePasswordBlur}
                   required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  className="block w-full rounded-md border-0 py-1.5 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
+                {passwordInputError && (
+                  <p className="text-[13px] text-red-500">
+                    {passwordInputError}
+                  </p>
+                )}
+                <label
+                  htmlFor="email"
+                  className="block text-[12px] font-medium leading-6 text-gray-600"
+                >
+                  Please ensure your password is minimum 10 characters long and
+                  contains at least a number and a capitalised letter
+                </label>
               </div>
             </div>
 
