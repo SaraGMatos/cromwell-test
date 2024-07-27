f898dd635c40b4e81da817d8c5aef083f26f0675
SaraGarciaM <saragarciamatos@gmail.com>
2024-07-27 20:58:58 +0100

    Implements password and salting for createuser

--- backend/models/user.models.ts
+++ backend/models/user.models.ts
@@ -1,13 +1,57 @@
 import { db } from "../db/connection";
+import bcrypt from "bcrypt";
+
+interface User {
+  user_id: number;
+  username: string;
+  email: string;
+  password: string;
+}
+
+const fetchUserByEmail = (email: string): Promise<User | null> => {
+  return db
+    .query(
+      `SELECT user_id, email, username, password FROM users WHERE email = $1`,
+      [email]
+    )
+    .then(({ rows }) => {
+      const user = rows[0];
+
+      if (rows.length === 0) {
+        return null;
+      }
+
+      return user;
+    });
+};
+
+const saveUser = (
+  username: string,
+  email: string,
+  hash: string
+): Promise<number> => {
+  return db
+    .query(
+      `INSERT INTO users (username, email, password)
+        VALUES ($1, $2, $3) RETURNING user_id`,
+      [username, email, hash]
+    )
+    .then(({ rows }) => {
+      console.log(rows);
+      const { user_id } = rows[0];
+
+      console.info(`Added user '${username}' in db`);
+      return user_id as number;
+    });
+};
 
 export const createUser = (
   username: string,
   email: string,
   password: string
-): Promise<number> => {
-  const formattedUser = [username, email, password];
+) => {
   console.info(`Adding user '${username}' to db`);
-  //! Below is redundant? The following check should fire the error when checking the type of a missing field
+
   if (!username || !email || !password) {
     console.error(`Could not add user '${username}' to db`);
     return Promise.reject({ status: 400, message: "Bad request." });
@@ -28,41 +72,14 @@ export const createUser = (
       }
     })
     .then(() => {
-      return db
-        .query(
-          `INSERT INTO users (username, email, password)
-    VALUES ($1, $2, $3) RETURNING user_id`,
-          formattedUser
-        )
-        .then(({ rows }) => {
-          console.log(rows[0].user_id);
-          const { user_id } = rows[0];
-
-          console.info(`Added user '${username}' in db`);
-          return user_id as number;
-        });
-    });
-};
-
-const fetchUserByEmail = (email: string): Promise<User | null> => {
-  return db
-    .query(
-      `SELECT user_id, email, username, password FROM users WHERE email = $1`,
-      [email]
-    )
-    .then(({ rows }) => {
-      const user = rows[0];
-
-      if (rows.length === 0) {
-        return null;
-      }
-
-      return user;
+      return bcrypt.hash(password, 10);
+    })
+    .then((hash) => {
+      return saveUser(username, email, hash);
     });
 };
 
 export const logInUser = (email: string, password: string): Promise<number> => {
-  //! Below is redundant? The following check should fire the error when checking the type of a missing field
   if (!email || !password) {
     return Promise.reject({ status: 400, message: "Bad request." });
   }
@@ -82,21 +99,15 @@ export const logInUser = (email: string, password: string): Promise<number> => {
   });
 };
 
-interface User {
-  user_id: number;
-  username: string;
-  email: string;
-  password: string;
-}
-
 export const fetchUserById = (user_id: number): Promise<User> => {
   console.info(`Fetching user '${user_id}' from db`);
+
   return db
     .query<User>(`SELECT username, email FROM users WHERE user_id = $1`, [
       user_id,
     ])
     .then(({ rows }) => {
-      const userObject = rows[0];
+      const user = rows[0];
 
       if (rows.length === 0) {
         console.error(`Could not find user '${user_id}' in db`);
@@ -105,6 +116,6 @@ export const fetchUserById = (user_id: number): Promise<User> => {
       }
 
       console.info(`Found user '${user_id}' in db`);
-      return userObject;
+      return user;
     });
 };
