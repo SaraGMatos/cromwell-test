e80d76aec84d44fabf848af305e30a798cdb4c5c
SaraGarciaM <saragarciamatos@gmail.com>
2024-07-27 14:09:51 +0100

    Adds some validation and types

--- frontend/src/components/LoginPage.tsx
+++ frontend/src/components/LoginPage.tsx
@@ -1,4 +1,53 @@
-export function LoginPage() {
+import { ChangeEvent, FormEvent, useEffect, useState } from "react";
+import { loginUser } from "../api";
+
+export function LoginPage(): JSX.Element {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [emailInputError, setEmailInputError] = useState("");
+  const [passwordInputError, setPasswordInputError] = useState("");
+
+  const [isButtonDisabled, setIsButtonDisabled] = useState(true);
+
+  function handleSubmit(event: FormEvent): void {
+    event?.preventDefault();
+    loginUser(email, password);
+  }
+
+  function handleEmailBlur(): void {
+    const emailRegexPattern = /^[\w-\.]+@([\w-]+\.)+[\w]{2,4}$/g;
+
+    if (!emailRegexPattern.test(email)) {
+      setIsButtonDisabled(true);
+      setEmailInputError("Invalid email.");
+    } else {
+      setIsButtonDisabled(false);
+      setEmailInputError("");
+    }
+  }
+
+  function handlePasswordBlur(): void {
+    const passwordRegexPattern =
+      /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{10,}$/g;
+
+    if (!passwordRegexPattern.test(password)) {
+      setIsButtonDisabled(true);
+      setPasswordInputError("Invalid password.");
+    } else {
+      setIsButtonDisabled(false);
+      setPasswordInputError("");
+    }
+  }
+
+  useEffect(() => {
+    if (emailInputError || passwordInputError) {
+      setIsButtonDisabled(true);
+    } else {
+      setIsButtonDisabled(false);
+    }
+  }, [isButtonDisabled]);
+
   return (
     <section className="h-full bg-white">
       <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
@@ -14,7 +63,7 @@ export function LoginPage() {
         </div>
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form className="space-y-6" action="#" method="POST">
+          <form className="space-y-6" onSubmit={handleSubmit}>
             <div>
               <label
                 htmlFor="email"
@@ -26,12 +75,19 @@ export function LoginPage() {
                 <input
                   id="email"
                   name="email"
-                  type="email"
+                  value={email}
+                  onChange={(event: ChangeEvent<HTMLInputElement>) => {
+                    setEmail(event.target.value);
+                  }}
+                  onBlur={handleEmailBlur}
                   autoComplete="email"
                   required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  className="block w-full rounded-md border-0 py-1.5 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
+              {emailInputError && (
+                <p className="text-[13px] text-red-500">{emailInputError}</p>
+              )}
             </div>
 
             <div>
@@ -48,16 +104,26 @@ export function LoginPage() {
                   id="password"
                   name="password"
                   type="password"
-                  autoComplete="current-password"
+                  value={password}
+                  onChange={(event: ChangeEvent<HTMLInputElement>) => {
+                    setPassword(event.target.value);
+                  }}
+                  onBlur={handlePasswordBlur}
                   required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  className="block w-full rounded-md border-0 py-1.5 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
+                {passwordInputError && (
+                  <p className="text-[13px] text-red-500">
+                    {passwordInputError}
+                  </p>
+                )}
               </div>
             </div>
 
             <div>
               <button
                 type="submit"
+                disabled={isButtonDisabled}
                 className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Sign in
