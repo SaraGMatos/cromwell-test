efde8f0514d02febd2c40020c233cd76568fe990
SaraGarciaM <saragarciamatos@gmail.com>
2024-07-27 17:27:01 +0100

    Adds home page route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import { LoginPage } from "./components/LoginPage";
 import { RegisterPage } from "./components/RegisterPage";
+import { HomePage } from "./components/HomePage";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/home/user/:id" element={<HomePage />} />
       </Routes>
     </>
   );
