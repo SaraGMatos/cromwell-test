107b736ec0ceeab1d26ed994c69dc2e26f1b7745
SaraGarciaM <saragarciamatos@gmail.com>
2024-07-27 16:16:42 +0100

    Adds some routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,16 @@
+import { Routes, Route } from "react-router-dom";
 import { LoginPage } from "./components/LoginPage";
-// import { RegisterPage } from "./components/RegisterPage";
+import { RegisterPage } from "./components/RegisterPage";
 
 function App() {
-  return <LoginPage />;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
