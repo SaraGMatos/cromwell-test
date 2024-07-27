f6e2afd1bd03b391d8bac63c754f911672b6b509
SaraGarciaM <saragarciamatos@gmail.com>
2024-07-27 13:28:38 +0100

    Creates connection to backend with axios

---
+++ frontend/src/api.ts
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+const cromwellApi = axios.create({
+  baseURL: "http://localhost:3000",
+});
+
+export const createUser = (
+  username: string,
+  email: string,
+  password: string
+) => {
+  return cromwellApi.post("/user/register", {
+    username: username,
+    email: email,
+    password: password,
+  });
+};
+
+export const loginUser = (email: string, password: string) => {
+  return cromwellApi.post("/user/login", {
+    email: email,
+    password: password,
+  });
+};
+
+export const getUser = (id: number) => {
+  return cromwellApi.get(`/user/${id}`);
+};
