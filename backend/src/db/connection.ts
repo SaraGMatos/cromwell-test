0b334df2bca2871c41767b99b35e521996b65575
SaraGarciaM <saragarciamatos@gmail.com>
2024-07-29 00:28:31 +0100

    Getting the backend ready for deployment in render

--- backend/src/db/connection.ts
+++ backend/src/db/connection.ts
@@ -1,10 +1,19 @@
-import { Pool } from "pg";
+import { Pool, PoolConfig } from "pg";
 import dotenv from "@dotenvx/dotenvx";
 
 dotenv.config();
+
+const ENV = process.env.NODE_ENV || "development";
 
 if (!process.env.PGDATABASE) {
   throw new Error("No PGDATABASE configured");
 }
 
-export const db = new Pool();
+const config: PoolConfig = {};
+
+if (ENV === "production") {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+}
+
+export const db = new Pool(config);
--- backend/src/db/data.ts
+++ backend/src/db/data.ts
@@ -2,26 +2,36 @@ export const userData = [
   {
     username: "John23",
     email: "john23@test.com",
-    password: "test",
+    // Password "Passwordtest1" hashed with 10 rounds using bcrypt
+    password:
+      "$2b$10$$2b$10$hXXhEs522.NlTXbqcJ8AVeKchADOYEEAxM/hurYodj6BQMQqwJ1uO.NGw8aOJzYvyiQnwOQMKo2GohjVyFZH6",
   },
   {
     username: "Lina",
     email: "lina@test.com",
-    password: "test",
+    // Password "Passwordtest1" hashed with 10 rounds using bcrypt
+    password:
+      "$2b$10$$2b$10$hXXhEs522.NlTXbqcJ8AVeKchADOYEEAxM/hurYodj6BQMQqwJ1uO.NGw8aOJzYvyiQnwOQMKo2GohjVyFZH6",
   },
   {
     username: "Mike",
     email: "mike@test.com",
-    password: "test",
+    // Password "Passwordtest1" hashed with 10 rounds using bcrypt
+    password:
+      "$2b$10$$2b$10$hXXhEs522.NlTXbqcJ8AVeKchADOYEEAxM/hurYodj6BQMQqwJ1uO.NGw8aOJzYvyiQnwOQMKo2GohjVyFZH6",
   },
   {
     username: "Diana",
     email: "diana@test.com",
-    password: "test",
+    // Password "Passwordtest1" hashed with 10 rounds using bcrypt
+    password:
+      "$2b$10$$2b$10$hXXhEs522.NlTXbqcJ8AVeKchADOYEEAxM/hurYodj6BQMQqwJ1uO.NGw8aOJzYvyiQnwOQMKo2GohjVyFZH6",
   },
   {
     username: "Carmen",
     email: "carmen@test.com",
-    password: "test",
+    // Password "Passwordtest1" hashed with 10 rounds using bcrypt
+    password:
+      "$2b$10$$2b$10$hXXhEs522.NlTXbqcJ8AVeKchADOYEEAxM/hurYodj6BQMQqwJ1uO.NGw8aOJzYvyiQnwOQMKo2GohjVyFZH6",
   },
 ];
