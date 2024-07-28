e19e0068bc840d67922815ecf19834f2363f42a9
SaraGarciaM <saragarciamatos@gmail.com>
2024-07-28 18:04:26 +0100

    Wraps homepage to protected route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,14 +2,22 @@ import { Routes, Route } from "react-router-dom";
 import { LoginPage } from "./components/LoginPage";
 import { RegisterPage } from "./components/RegisterPage";
 import { HomePage } from "./components/HomePage";
+import { ProtectedRoutes } from "./components/ProtectedRoutes";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoutes>
+              <HomePage />
+            </ProtectedRoutes>
+          }
+        />
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
-        <Route path="/home/user/:id" element={<HomePage />} />
       </Routes>
     </>
   );
