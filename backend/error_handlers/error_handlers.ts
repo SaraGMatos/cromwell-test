fb345e2e45029292fc332828fb30bde6db8b0b5e
SaraGarciaM <saragarciamatos@gmail.com>
2024-07-28 14:49:06 +0100

    Adds jwt handler

--- backend/error_handlers/error_handlers.ts
+++ backend/error_handlers/error_handlers.ts
@@ -1,11 +1,20 @@
 import { ErrorRequestHandler } from "express";
+import { JsonWebTokenError } from "jsonwebtoken";
 
 export const sendCustomError: ErrorRequestHandler = (err, req, res, next) => {
   if (err.status && err.message) {
     res.status(err.status).send({ message: err.message });
   }
 
-  next(JSON.stringify(err));
+  next(err);
+};
+
+export const sendJWTError: ErrorRequestHandler = (err, req, res, next) => {
+  if (err instanceof JsonWebTokenError) {
+    res.status(403).send({ message: "Unauthorised" });
+  }
+
+  next(err);
 };
 
 export const sendServerError: ErrorRequestHandler = (err, req, res, next) => {
