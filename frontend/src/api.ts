62bf79ccc4ee1ba30a90d3a5b9ee2a5f5d26a867
SaraGarciaM <saragarciamatos@gmail.com>
2024-07-30 21:31:11 +0100

    Add a new branch for production

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const cromwellApi = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://cromwelltest.netlify.app",
 });
 
 export const createUser = (
